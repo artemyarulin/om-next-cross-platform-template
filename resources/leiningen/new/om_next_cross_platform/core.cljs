31b53ba88e872e3659962d1b93c87379bf7ca91b
Artem Yarulin <artem.yarulin@fessguid.com>
2016-01-03 14:23:27 +0200

    Fixed wrong imports from the latest ktoa lib

--- resources/leiningen/new/om_next_cross_platform/core.cljs
+++ resources/leiningen/new/om_next_cross_platform/core.cljs
@@ -1,7 +1,9 @@
 (ns {{name}}.core
   (:require [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
-            [ktoa.core :as ktoa :refer [view text text-input]]))
+            [ktoa.components :refer [view text]]
+            [ktoa.core :as ktoa]
+            [ktoa.om :as ktoa-om]))
 
 (def status "Hello cross-platform world!")
 
@@ -18,12 +20,12 @@
 (defui RootComponent
   Object
   (render [this]
-          (if ktoa/react-native?
+          (if ktoa/react-native
             (render-mobile)
             (render-browser))))
 
 (ktoa/register! "RootViewRN"
-                #(om/add-root! (om/reconciler ktoa/om-options)
+                #(om/add-root! (om/reconciler ktoa-om/om-options)
                                RootComponent
                                %)
                 #(.querySelector js/document "#app"))
